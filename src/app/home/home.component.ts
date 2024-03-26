d50c036343c06210f2989ae4c730cf58a51ae09c
Shotblkr29 <shotblkr29@aol.com>
2024-03-26 19:41:29 -0400

    commit removing url from template and style

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'bot-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  template: `<p class="red">`,
+  styles: [`.home.component.css`]
 })
 export class HomeComponent {
 
