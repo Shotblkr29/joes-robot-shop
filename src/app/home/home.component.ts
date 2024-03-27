2a848d938978382593f94b9bf2e9a2e266a5704d
Shotblkr29 <shotblkr29@aol.com>
2024-03-26 20:16:36 -0400

    Updating template command line commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'bot-home',
-  templateUrl: './home.component.html',
+  template: '<p class="red">Inline home works!</p>'
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
