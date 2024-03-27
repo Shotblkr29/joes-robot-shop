1c346720a3909119633406ff0d406f35a8ec4e10
Shotblkr29 <shotblkr29@aol.com>
2024-03-26 20:18:41 -0400

    added string to template command

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'bot-home',
-  template: '<p class="red">Inline home works!</p>'
+  template: `
+  <p class="red">
+    Inline home works!
+  </p>``
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
